7e66649b2275482192ff56a2cbe981bf1f451b9b
Lily Egg <lyeggleston@gmail.com>
2021-07-20 18:19:30 -0400

    after video23. added sprite animations

--- js/Entities.js
+++ js/Entities.js
@@ -1,35 +1,39 @@
 var player;
-var enemyList = {};
-var upgradeList = {};
-var bulletList = {};
 
-Entity = function (type, id, x, y, spdX, spdY, width, height, color) {
+
+
+
+Entity = function (type, id, x, y, width, height, img) {
 	var self = {
 		type: type,
 		id:id,
 		x : x, 
-		spdX : spdX,
 		y : y, 
-		spdY : spdY,
 		width: width, 
 		height: height,
-		color: color,
+		img: img,
 		};
-		self.update = function() {
-			self.updatePosition();
-			self.draw();
-		}
-		self.updatePosition = function() {
-			self.x += self.spdX;
-			self.y += self.spdY;
-					
-			if(self.x < 0 || self.x > WIDTH){
-				self.spdX = -self.spdX;
-			}
-			if(self.y < 0 || self.y > HEIGHT){
-				self.spdY = -self.spdY;
-			}
-		}
+	self.update = function() {
+		self.updatePosition();
+		self.draw();
+	}
+	self.draw = function() {
+		ctx.save();
+	
+		var x = self.x - player.x;
+		var y = self.y - player.y;
+	
+	x += WIDTH/2;
+	y += HEIGHT/2;
+	
+	x -= self.width/2;
+	y -= self.height/2;
+	
+	ctx.drawImage(self.img, 0, 0, self.img.width, self.img.height, x, y, self.width, self.height);
+	
+	ctx.restore();
+	}
+	
 	self.getDistance= function ( entity2) {   //return distance (number)
 	var vx = self.x - entity2.x;
 	var vy = self.y - entity2.y;
@@ -49,35 +53,74 @@ Entity = function (type, id, x, y, spdX, spdY, width, height, color) {
 		height:entity2.height,
 	}
 	return textCollisionRectRect(rect1, rect2);
-};
-self.draw = function() {
-	ctx.save();
-	ctx.fillStyle =  self.color;
-	ctx.fillRect(self.x-self.width/2, self.y-self.height/2, self.width,self.height );
-	ctx.restore();
-	}
+}
+	self.updatePosition = function(){}
 	return self;
 	}
 
-
-
-Actor = function (type, id, x, y, spdX, spdY, width, height,color, hp, atkSpd) {
- var self = Entity(type, id, x, y, spdX, spdY, width, height, color);
+Actor = function (type, id, x, y, width, height,img, hp, atkSpd) {
+ var self = Entity(type, id, x, y, width, height, img);
  
  self.hp = hp;
+ self.hpMax = hp;
  self.attackCounter = 0;
  self.aimAngle = 0;
  self.atkSpd = atkSpd;
- 
+
+ self.spriteAnimCounter = 0;
+
+ self.draw = function() {
+	ctx.save();
+
+	var x = self.x - player.x;
+	var y = self.y - player.y;
+
+x += WIDTH/2;
+y += HEIGHT/2;
+
+x -= self.width/2;
+y -= self.height/2;
+
+var frameWidth = self.img.width/3;
+var frameHeight = self.img.height/4;
+
+var aimAngle = self.aimAngle;
+if(aimAngle < 0) 
+	aimAngle = 360 + aimAngle;
+
+//if(type === 'player') 
+//	console.log(type + ' aimAngle = ' + aimAngle  )
+
+var directionMod = 2;
+if(aimAngle >= 45 && aimAngle < 135)
+	directionMod = 0;
+else if(aimAngle >= 135 && aimAngle < 225)
+	directionMod = 1;
+else if(aimAngle >= 225 && aimAngle < 315)
+	directionMod = 3;
+
+var walkingMod = Math.floor(self.spriteAnimCounter) % 3;
+
+ctx.drawImage(self.img, walkingMod*frameWidth, directionMod*frameHeight, frameWidth, frameHeight, x, y, self.width, self.height);
+
+
+ctx.restore();
+}
+
  var super_update = self.update;
  self.update = function () {
 	super_update();
 	self.attackCounter += self.atkSpd;
+	
+		if(self.hp <= 0) 
+			self.onDeath();
 }
+self.onDeath = function(){};
+
  self.performAttack = function() {
 	if(self.attackCounter > 25){	//every 1 sec
 			self.attackCounter = 0;
-			generateBullet(self);
+			bullet.generate(self);
 	}
 }
 	self.performSpecialAttack = function() {
@@ -85,166 +128,256 @@ Actor = function (type, id, x, y, spdX, spdY, width, height,color, hp, atkSpd) {
 			self.attackCounter = 0;
 			/*
 			for(var i = 0 ; i < 360; i++){
-				generateBullet(self,i);
+				bullet.generate(self,i);
 			}
 			*/
-			generateBullet(self,self.aimAngle - 5);
-			generateBullet(self,self.aimAngle);
-			generateBullet(self,self.aimAngle + 5);
+			bullet.generate(self,self.aimAngle - 5);
+			bullet.generate(self,self.aimAngle);
+			bullet.generate(self,self.aimAngle + 5);
 			}
 		}
 	
 return self;
 }
 
-Player = function() {
-var self = Actor('player', 'myId', 50, 40, 30, 5, 20, 20, 'green', 10, 1)
+Player = function () {
 
-self.updatePosition = function() {
+	var self = Actor('player', 'myId', 50, 40, 50, 45, Img.player, 10, 1)
 
-				if(self.pressingRight)
-					self.x += 10;
-				if(self.pressingLeft)
-					self.x -= 10;	
-				if(self.pressingDown)
-					self.y += 10;	
-				if(self.pressingUp)
-					self.y -= 10;	
- 
-				//ispositionvalid
-				if(self.x < self.width/2)
-					self.x = self.width/2;
-				if(self.x > WIDTH-self.width/2)
-					self.x = WIDTH - self.width/2;
-				if(self.y < self.height/2)
-					self.y = self.height/2;
-				if(self.y > HEIGHT - self.height/2)
-					self.y = HEIGHT - self.height/2;
-			}
-		var super_update = self.update;
-
-		self.update = function () {
+	var super_update = self.update;
+	self.update = function () {
 		super_update();
-		if (self.hp <= 0) {
-			var timeSurvived = Date.now() - timeWhenGameStarted;
-			
-			console.log('You lost! You survived for ' + timeSurvived + 'ms');
-			
-			startNewGame();
-			}
-		}
+		if(self.pressingRight || self.pressingLeft || self.pressingDown || self.pressingUp)
+			self.spriteAnimCounter += 0.2;
+		if (self.pressingMouseLeft)
+			self.performAttack();
+		if (self.pressingMouseRight)
+			self.performSpecialAttack();
+	}
 
-	self.pressingDown=false;
-	self.pressingUp= false;
-	self.pressingLeft= false;
-	self.pressingRight= false;
+	self.updatePosition = function () {
+
+		if (self.pressingRight)
+			self.x += 10;
+		if (self.pressingLeft)
+			self.x -= 10;
+		if (self.pressingDown)
+			self.y += 10;
+		if (self.pressingUp)
+			self.y -= 10;
+
+		//ispositionvalid
+		if (self.x < self.width / 2)
+			self.x = self.width / 2;
+		if (self.x > currentMap.width - self.width / 2)
+			self.x = currentMap.width - self.width / 2;
+		if (self.y < self.height / 2)
+			self.y = self.height / 2;
+		if (self.y > currentMap.height - self.height / 2)
+			self.y = currentMap.height - self.height / 2;
+	}
+	self.onDeath = function () {
+		var timeSurvived = Date.now() - timeWhenGameStarted;
+		console.log('You lost! You survived for ' + timeSurvived + 'ms');
+		startNewGame();
+	}
+
+	self.pressingDown = false;
+	self.pressingUp = false;
+	self.pressingLeft = false;
+	self.pressingRight = false;
+
+	self.pressingMouseLeft = false;
+	self.pressingMouseRight = false;
 	return self;
 
 }
  
 
-Enemy = function (id,x, y, spdX, spdY, width, height) {
-	var self = Actor('enemy', id, x, y, spdX, spdY, width, height, 'red', 10, 1) 
+Enemy = function (id,x, y, width, height, img, hp, atkSpd) {
+	var self = Actor('enemy', id, x, y, width, height, img, hp, atkSpd) 
+	Enemy.list[id] = self;
+	
+	self.toRemove = false;
+	
 	var super_update = self.update;
 	self.update = function() {
-	super_update();
-	self.performAttack();
-	
-	var isColliding = player.testCollision(self);
-	if(isColliding){
-		player.hp = player.hp - 1;
-		
-		}
-		}
-	enemyList[id] = self;
-};
+		super_update();
+		self.spriteAnimCounter += 0.2;
+		self.updateAim();
+		self.performAttack();
+	}
+	self.updateAim = function() {
+		var diffX = player.x - self.x;
+		var diffY = player.y - self.y;
 
-randomlyGenerateEnemy = function() {
-	var x  = Math.random()*WIDTH;
-	var y = Math.random()*HEIGHT;
-	var height = 10 + Math.random()*30;
-	var width = 10 + Math.random()*30;
+		self.aimAngle = Math.atan2(diffY, diffX) / Math.PI *180
+	}
+	var super_draw = self.draw;
+	self.draw = function() {
+		super_draw();
+			var x = self.x - player.x + WIDTH/2;
+			var y = self.y - player.y + HEIGHT/2 - self.height/2 - 20;
+		ctx.save();
+		ctx.fillStyle = 'red';
+		var width = 100*self.hp/self.hpMax;
+		if(width < 0) 
+			width = 0;
+		ctx.fillRect(x-50, y, width, 10);
+		
+		ctx.strokeStyle = 'black';
+		ctx.strokeRect(x-50, y, 100, 10);
+
+		ctx.restore();
+	}
+
+	self.onDeath = function(){
+		self.toRemove = true;
+	}
+	
+	self.updatePosition = function(){
+		var diffX = player.x - self.x;
+		var diffY = player.y - self.y;
+
+		if(diffX > 0) 
+			self.x += 3;
+		else 
+			self.x -= 3;
+		
+		if(diffY > 0)
+			self.y += 3;
+		else
+			self.y -= 3;
+	}
+};
+Enemy.list = {};
+Enemy.update = function() {
+	if(frameCount % 100 === 0) //every 4 sec 
+	Enemy.randomlyGenerate();
+for(var key in Enemy.list) {
+	Enemy.list[key].update();
+}
+for(var key in Enemy.list) {
+	if(Enemy.list[key].toRemove)
+	 delete Enemy.list[key]
+}
+}
+Enemy.randomlyGenerate = function() {
+	var x  = Math.random()*currentMap.width;
+	var y = Math.random()*currentMap.height;
+	var height = 70
+	var width = 70
 	var id = Math.random();
-	var spdX = 5 + Math.random() * 7;
-	var spdY = 5 + Math.random() * 7;
-	Enemy(id,x, y,spdX,  spdY,  width, height);
+	if(Math.random() <0.5)
+		Enemy(id,x, y, width, height, Img.goat, 3, 1);
+	else
+		Enemy(id,x, y, width, height, Img.crow, 1, 3);
 }
 
-upgrade = function ( id,x, y, spdX, spdY,  width, height, color, category) {
-var self = Entity('upgrade', id, x, y, spdX, spdY, width, height, color) 
+upgrade = function ( id,x, y,  width, height, img, category) {
+var self = Entity('upgrade', id, x, y, width, height, img) 
 
-var super_update =  self.update;
-self.update = function() {
-super_update();
-var isColliding = player.testCollision( self);
+	self.category = category,
+	upgrade.list[id] = self;
+}
+upgrade.list = {};
+upgrade.update = function() {
+	if(frameCount % 75 === 0) 
+		upgrade.randomlyGenerate();
+	for(var key in upgrade.list) {
+	upgrade.list[key].update();
+	var isColliding = player.testCollision( upgrade.list[key]);
 	if(isColliding){
-		if(self.category === 'score') 
+		if(upgrade.list[key].category === 'score') 
 			score += 100;
-		if(self.category=== 'atkSpd')  
+		if(upgrade.list[key].category=== 'atkSpd')  
 			player.atkSpd += 3;
 		
-		delete upgradeList[self.id];
+		delete upgrade.list[key];
 		
 		}
-		}
-	self.category = category,
-	upgradeList[id] = self;
-}
-randomlyGenerateUpgrade= function() {
-	var x  = Math.random()*WIDTH;
-	var y = Math.random()*HEIGHT;
-	var height = 10 ;
-	var width = 10 ;
+	}
+}	
+
+upgrade.randomlyGenerate= function() {
+	var x  = Math.random()*currentMap.width;
+	var y = Math.random()*currentMap.height;
+	var height = 20 ;
+	var width = 32 ;
 	var id = Math.random();
-	var spdX = 0;
-	var spdY = 0;
 	
 	if (Math.random() <0.5) {
 		var category = 'score';
-		var color = 'orange';
+		var img = Img.update1;
 	}else { 
 		var category = 'atkSpd';
-		var color = 'purple';
+		var img = Img.update2;
 	}
 	
-	upgrade(id, x, y, spdX, spdY, width, height, color ,category);
+	upgrade(id, x, y, width, height, img ,category);
 }
 
-bullet = function (id,x, y,spdX,  spdY,  width, height) {
-var self = Entity('bullet', id, x, y, spdX, spdY, width, height, 'black') 
+bullet = function (id,x, y, spdX, spdY, width, height, combatType) {
+var self = Entity('bullet', id, x, y, width, height, Img.bullet) 
 	self.timer = 0;
+	self.combatType = combatType;
+	self.spdX = spdX;
+	self.spdY = spdY;
+
+	self.updatePosition =  function() {
+			self.x += self.spdX;
+			self.y += self.spdY;
+					
+			if(self.x < 0 || self.x > currentMap.width){
+				self.spdX = -self.spdX;
+			}
+			if(self.y < 0 || self.y > currentMap.height){
+				self.spdY = -self.spdY;
+			}
+	}
+
+	bullet.list[id] = self;
+}
+bullet.list = {};
+bullet.update = function() {
+	for(var key in bullet.list) {
+		var b  = bullet.list[key];
+		b.update();
 	
-	var super_update = self.update;
-	self.update = function() {
-	super_update();
-	var toRemove = false;
-		self.timer++;
-		if(self.timer > 75) {
+		var toRemove = false;
+		b.timer++;
+		if(b.timer > 75) {
 			toRemove = true;
 		}
-	
-		for(var key2 in enemyList) {
-			/*
-			var isColliding = self.testCollision(enemyList[key2]);
-			if(isColliding){
-				toRemove= true;
-				delete enemyList[key2];
-				break;
+		if(b.combatType === 'player'){ //bullet was shot by player
+			for(var key2 in Enemy.list){
+				
+				if(b.testCollision(Enemy.list[key2])){
+					toRemove= true;
+					Enemy.list[key2].hp -= 1;
+					break;
 				}
-				*/
-		}
-		if(toRemove) {
-			delete bulletList[self.id];
 			}
-			
-	}
-	bulletList[id] = self;
+		} else if(b.combatType === 'enemy') {
+			if(b.testCollision(player)){
+				toRemove = true;
+				player.hp -= 1;
+			}
+		}
+
+
+		if(toRemove) {
+			delete bullet.list[self.id];
+		}
+
+		
+ 	}
 }
-generateBullet= function(actor, aimOverwrite) {
+bullet.generate= function(actor, aimOverwrite) {
 	var x  = actor.x;
 	var y = actor.y;
-	var height = 10 ;
-	var width = 10 ;
+	var height = 25 ;
+	var width = 25 ;
 	var id = Math.random();
 	
 	var angle;
@@ -254,5 +387,5 @@ generateBullet= function(actor, aimOverwrite) {
 	
 	var spdX = Math.cos(angle/180*Math.PI)*5;
 	var spdY = Math.sin(angle/180*Math.PI)*5;
-	bullet(id,x, y, spdX, spdY, width, height);
+	bullet(id,x, y, spdX, spdY, width, height, actor.type);
 }
